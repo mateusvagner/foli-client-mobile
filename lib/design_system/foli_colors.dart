70ec4f32c778d4fabeab955e3f3bee02372f24c5
Mateus Vagner <mateus.vagner@gmail.com>
2022-09-10 18:50:29 -0300

    feat: create some first foli colors

---
+++ lib/design_system/foli_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color foliPrimaryColor = Color(0xFF22AF5D);
+
+const Color foliGreyMediumColor = Color(0xFF868686);
+const Color foliGreyLightColor = Color(0xFFE5E5E5);
+const Color foliGreyExtraLightColor = Color(0xFFF2F2F2);
+
