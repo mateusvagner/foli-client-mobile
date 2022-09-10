4d178948c3d161698196cdcc6bffc7591c114c64
Mateus Vagner <mateus.vagner@gmail.com>
2022-09-10 18:56:58 -0300

    style: add EOF line

--- lib/design_system/foli_colors.dart
+++ lib/design_system/foli_colors.dart
@@ -5,4 +5,3 @@ const Color foliPrimaryColor = Color(0xFF22AF5D);
 const Color foliGreyMediumColor = Color(0xFF868686);
 const Color foliGreyLightColor = Color(0xFFE5E5E5);
 const Color foliGreyExtraLightColor = Color(0xFFF2F2F2);
-
