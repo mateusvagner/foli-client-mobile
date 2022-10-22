23bfe3afc16cb43a732b615b77ecfb50bd19fd81
Mateus Vagner <mateus.vagner@gmail.com>
2022-10-22 16:24:32 -0300

    feat: add endpoint to get token

--- lib/service/dio_impl/dio_user_service.dart
+++ lib/service/dio_impl/dio_user_service.dart
@@ -2,9 +2,10 @@ import 'package:dio/dio.dart';
 
 import 'package:foli_client_mobile/resource/new_user_resource.dart';
 import 'package:foli_client_mobile/service/user_service.dart';
+import 'package:foli_client_mobile/shared_preferences_keys.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class DioUserService implements UserService {
-
   final Dio _dio;
 
   DioUserService(this._dio);
@@ -18,6 +19,27 @@ class DioUserService implements UserService {
       );
 
       return NewUserResource.fromMap(response.data);
+    } catch (e) {
+      // TODO implement error handling
+      return null;
+    }
+  }
+
+  @override
+  Future<String?> getAccessToken(String email, String password) async {
+    try {
+      var response = await _dio.post(
+        '/user/login',
+        data: {'email': email, 'password': password},
+        options: Options(contentType: Headers.formUrlEncodedContentType),
+      );
+
+      var token = response.data as String;
+
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      prefs.setString(SharedPreferencesKeys.jwtToken.name, token);
+
+      return response.data as String;
     } catch (e) {
       // TODO implement error handling
       return null;
--- lib/service/user_service.dart
+++ lib/service/user_service.dart
@@ -1,9 +1,9 @@
-import 'package:foli_client_mobile/service/user_service_url.dart';
-
 import '../resource/new_user_resource.dart';
 
 abstract class UserService {
 
   Future<NewUserResource?> postNewUser(NewUserResource newUserResource);
+
+  Future<String?> getAccessToken(String email, String password);
 
 }
