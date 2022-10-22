eea593257f0f359d76aa54beb71d9c2a49d53fb2
Mateus Vagner <mateus.vagner@gmail.com>
2022-10-22 15:10:20 -0300

    feat: create RefreshTokenInterceptor

---
+++ lib/service/interceptor/refresh_token_interceptor.dart
@@ -0,0 +1,49 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../shared_preferences_keys.dart';
+
+class RefreshTokenInterceptor  extends Interceptor {
+
+  @override
+  Future onRequest(RequestOptions options, RequestInterceptorHandler handler) async {
+    if (kDebugMode) {
+      print('REQUEST[${options.method}] => PATH: ${options.path}');
+    }
+
+    if (options.headers.containsKey("requiresAuthorization")) {
+      options.headers.remove("requiresAuthorization");
+
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      var token = prefs.get(SharedPreferencesKeys.jwtToken.name);
+
+      options.headers.addAll({'Authorization': 'Bearer $token'});
+
+      return handler.next(options);
+    }
+
+    return super.onRequest(options, handler);
+  }
+
+  @override
+  Future onResponse(Response response, ResponseInterceptorHandler handler) async {
+    if (kDebugMode) {
+      print('RESPONSE[${response.statusCode}] => PATH: ${response.requestOptions.path}');
+    }
+
+    // TODO Do something with response data
+
+    return super.onResponse(response, handler);
+  }
+  @override
+  Future onError(DioError err, ErrorInterceptorHandler handler) async {
+    if (kDebugMode) {
+      print('ERROR[${err.response?.statusCode}] => PATH: ${err.requestOptions.path}');
+    }
+
+    // TODO Do something with response error
+
+    return super.onError(err, handler);
+  }
+}
